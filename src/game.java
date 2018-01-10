655a2d603b8df963148931e890dbc9f03baefc53
Koen Alblas <koen.alblas@gmail.com>
2018-01-10 22:20:02 +0100

    1 ship on 1 line

--- src/game.java
+++ src/game.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
 
 public class game {
 	public static void main (String [] args) {
-		int [] intArray = {0,0,0,0,3,3,3,0,0};
-	
+		int [] intArray = {0, 0, 0, 0, 3, 3, 3, 0, 0, 0};
+	//veld van 10 plekken met een schip van lengte 3
+		System.out.println(Arrays.toString(intArray));
+		//print het veld met schip
 	}
-	
 
-}
+	}
+
+	
