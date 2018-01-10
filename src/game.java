284bdb890b0bbdb487c9662e2db8dfa74d6ee090
Koen Alblas <koen.alblas@gmail.com>
2018-01-10 22:53:35 +0100

    schieten

--- src/game.java
+++ src/game.java
@@ -1,13 +1,31 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class game {
 	public static void main (String [] args) {
-		int [] intArray = {0, 0, 0, 0, 3, 3, 3, 0, 0, 0};
+		int [] intArray = {1, 0, 0, 0, 3, 3, 3, 0, 0, 0};
 	//veld van 10 plekken met een schip van lengte 3
 		System.out.println(Arrays.toString(intArray));
 		//print het veld met schip
+		if (intArray[0] == 0) {System.out.println("missed");
+		
+	//	Scanner sc = new Scanner(System.in);
+	//	for (int j = 0; j < 10; j++) {
+	//		String i = sc.nextLine();
+	//		checkIfShipHit(i);
 	}
+	}
+}
+//	}
 
-	}
+//	public static void checkIfShipHit(String i) {
+//		if (intArray[0] == 0)
+//		//input x moet worden vergeleken met array nummer
+//			
+//		} else {
+//			System.out.println("nee, komt niet voor");
+
+//		}
+//	}
 
 	
