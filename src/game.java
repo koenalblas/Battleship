bfb5a0f7a24932c2364fff674df4ef8e2b7e29d3
Koen Alblas <koen.alblas@gmail.com>
2018-01-10 22:57:35 +0100

    raak

--- src/game.java
+++ src/game.java
@@ -7,15 +7,17 @@ public class game {
 	//veld van 10 plekken met een schip van lengte 3
 		System.out.println(Arrays.toString(intArray));
 		//print het veld met schip
-		if (intArray[0] == 0) {System.out.println("missed");
-		
+		if (intArray[5] == 0) {System.out.println("missed");}
+		 else {
+			System.out.println("hit");}
+		 
 	//	Scanner sc = new Scanner(System.in);
 	//	for (int j = 0; j < 10; j++) {
 	//		String i = sc.nextLine();
 	//		checkIfShipHit(i);
 	}
 	}
-}
+
 //	}
 
 //	public static void checkIfShipHit(String i) {
@@ -27,5 +29,3 @@ public class game {
 
 //		}
 //	}
-
-	
