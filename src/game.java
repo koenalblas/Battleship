bc05b56dbcf4717bfe2c8117ec4e22c63b99548a
Koen Alblas <koen.alblas@gmail.com>
2018-01-11 00:19:48 +0100

    mutli array
    mutli array begin

--- src/game.java
+++ src/game.java
@@ -2,11 +2,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class game {
-	public static int [] intArray = {1, 0, 0, 0, 3, 3, 3, 0, 0, 0};
+	public static int [][] Sea = 	{
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 3, 3, 3, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+										};
 	public static void main (String [] args) {
-		int [] intArray = {1, 0, 0, 0, 3, 3, 3, 0, 0, 0};
+	//	int [] Sea = {1, 0, 0, 0, 3, 3, 3, 0, 0, 0};
 	//veld van 10 plekken met een schip van lengte 3
-		System.out.println(Arrays.toString(intArray));
+		System.out.println(Arrays.toString(Sea));
 		//print het veld met schip
 		 
 	Scanner sc = new Scanner(System.in);
@@ -17,7 +28,7 @@ public class game {
 	}
 	
 	public static void checkIfShipHit(int i2) {
-		if (intArray[i2] == 0) 
+		if (Sea[i2] == 0) 
 			{System.out.println("missed");}
 		else 
 			{System.out.println("hit");}
